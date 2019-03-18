f08e7c700bafd3c314d39aa9facc3f4a61a8844d
Daniel Fries <daniel.w.fries@gmail.com>
2019-03-18 14:25:06 -0400

    passwords added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_160038) do
+ActiveRecord::Schema.define(version: 2019_03_18_175615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_160038) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "roles", "articles"
